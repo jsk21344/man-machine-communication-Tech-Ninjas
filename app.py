55ce4a2f14ee84f9851729b79e55b7ccd869c6c4
Leptopoda <rimikis.nikolas@gmail.com>
2022-05-12 09:44:56 +0000

    finally workin

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ from turbo_flask import Turbo
 import threading
 import time
 
-from operations import Operations
-from voice_assistant import VoiceAssistant
+# operations
+import time
+import math
+import mraa
+
+# assistant
+import pyttsx3 as tts
+import speech_recognition
 
 
 app = Flask(__name__)
@@ -19,6 +25,128 @@ global debug
 
 maschineID = 0
 debug = False
+
+
+class VoiceAssistant:
+    def init(self):
+        self.recognizer = speech_recognition.Recognizer()
+        self.microphone = speech_recognition.Microphone()
+        self.ttsEngine = tts.init()
+
+        self.setup()
+
+    def setup(self):
+        global debug
+
+        voices = self.ttsEngine.getProperty("voices")
+        self.ttsEngine.setProperty("voice", voices[8].id)
+        self.ttsEngine.setProperty("rate", 150)
+        if debug:
+            for voice in voices:
+                print(str(voice.id))
+            print(str(voices[8].id))
+
+    def speak(self, text_to_speech):
+        print(str(text_to_speech))
+        self.ttsEngine.say(str(text_to_speech))
+        self.ttsEngine.runAndWait()
+
+    def listen(self):
+        with self.microphone:
+            recognized_data = ""
+
+            self.recognizer.adjust_for_ambient_noise(
+                self.microphone, duration=2)
+
+            try:
+                print("Listening...")
+                audio = self.recognizer.listen(self.microphone, 5, 5)
+
+            except speech_recognition.WaitTimeoutError:
+                print("Can you check if your microphone is on, please?")
+                return
+
+            try:
+                print("Started recognition...")
+                recognized_data = self.recognizer.recognize_google(
+                    audio, language="de").lower()
+
+            except speech_recognition.UnknownValueError:
+                pass
+
+            except speech_recognition.RequestError:
+                print("Check your Internet Connection, please")
+
+            print("Listened: " + recognized_data)
+            return recognized_data
+
+
+class Operations:
+    def eingriff(self):
+        s = mraa.Aio(3)
+        x = mraa.I2c(0)
+        x.address(0x53)
+
+        x.writeReg(0x2D, 8)
+        time.sleep(0.01)
+        x.writeReg(0x1E, 1)
+        time.sleep(0.01)
+        x.writeReg(0x1F, 130)
+        time.sleep(0.01)
+        x.writeReg(0x20, 137)
+        time.sleep(0.01)
+
+        sensor_F = 0
+        x_out_F = 0
+        y_out_F = 0
+        z_out_F = 0
+        pitch_F = 0
+        roll_F = 0
+
+        while True:
+            x_out = (x.readReg(0x32) | x.readReg(0x33) << 8)
+            x_out = x_out / 25.6
+            y_out = (x.readReg(0x34) | x.readReg(0x35) << 8)
+            y_out = y_out / 25.6
+            z_out = (x.readReg(0x36) | x.readReg(0x37) << 8)
+            z_out = z_out / 25.6
+            sensor = s.read()
+
+            roll = math.atan(
+                y_out/math.sqrt(x_out**2+z_out**2))*180/math.pi
+            pitch = math.atan(-1*x_out /
+                              math.sqrt(y_out**2+z_out**2))*180/math.pi
+
+            roll_F = 0.5*roll_F+0.5*roll
+            pitch_F = 0.5 * pitch_F + 0.5 * pitch
+            x_out_F = 0.5 * x_out_F + 0.5 * x_out
+            y_out_F = 0.5 * y_out_F + 0.5 * y_out
+            z_out_F = 0.5 * z_out_F + 0.5 * z_out
+            sensor_F = 0.5 * sensor_F + 0.5 * sensor
+
+            print("Roll:" + str(roll_F) + " pitch: " + str(pitch_F) + " x_out: " + str(x_out_F) +
+                  " y_out: " + str(y_out_F) + " z_out: " + str(z_out_F) + "Sensor F: " + str(sensor_F))
+            time.sleep(0.1)
+
+    def status(self):
+        global assistant
+        global maschineID
+
+        if(maschineID == 0):
+            assistant.speak("Keine Maschine ausgewählt")
+        else:
+            assistant.speak("Maschine" + str(maschineID) +
+                            "läuft und hat keinen Fehler")
+
+    def select_machine(self, id):
+        global maschineID
+
+        maschineID = id
+
+    def start(self):
+        global assistant
+
+        assistant.speak("Maschine startet")
 
 
 @app.route('/')
@@ -51,6 +179,9 @@ def update_load():
 
 
 def main_start_assistant():
+    global assistant
+    global operations
+
     while True:
         voice_input = assistant.listen()
         voice_input = voice_input.split(" ")
@@ -69,11 +200,13 @@ def main_start_assistant():
 
 
 def main_init():
+    global assistant
+    global operations
+
     assistant = VoiceAssistant()
     assistant.init()
-    voice_input = "Maschine 1 Bauteil nicht greifbar. Bitte manuell greifen"
-
-    assistant.speak(voice_input)
+    assistant.speak("Maschine 1 Bauteil nicht greifbar. Bitte manuell greifen")
     operations = Operations()
 
     main_start_assistant()
+
